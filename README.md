0ec73169e3d1675ee26e540e441e17f3d06f52dc
wtmadaj <wtmadaj@users.noreply.github.com>
2019-06-09 06:41:30 -0400

    Minor changes

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SpringHello
 
 A simple Spring program
+
+To execute:
+    1: Run the Spring application
+    2: In Postman, do an HTTP GET operation to http://localhost:8080/{yourFirstNameHere}/{yourLastNameHere}
+    3: the JSON payload you see in Postman shows the return message of {"message":"Hello {firstName} {lastName}"}
--- src/main/java/com/github/wtmadaj/springhello/Application.java
+++ src/main/java/com/github/wtmadaj/springhello/Application.java
@@ -1,3 +1,5 @@
+package com.github.wtmadaj.springhello;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,11 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    /*
+    Exposes a single GET HTTP endpoint that takes two params - firstName, lastName
+    Returns JSON string containing a payload of the message "Hello" followed by firstName and lastName
+     */
 
     @RequestMapping(value = "/{firstName}/{lastName}", method = RequestMethod.GET)
     public String hello(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
